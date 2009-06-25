a219caddb3cb25e010e5d01422687e9fbd9fd874
Jason Tsao <jtsao22@gmail.com>
2009-06-25 13:19:24 -0700

    This includes all previous working functions + working generate function (but still needs added functionality) and the beginnings of the subset function within the Transaction class

--- apriori.py
+++ apriori.py
@@ -9,13 +9,16 @@ import string,sys
 
 # Transactions are sets of items
 class Transaction:
-    item_set = []
-    def __init__(self,item_set):
+    item_set = set([])              # used to hold the set of items
+    def __init__(self,item_set):    # function to initialize a
+                                    #Transaction
         self.item_set = item_set
 
     def subset(self, set_looking_for):
-        print 'This will be implemented later'
-
+        counter = 0                 # initialize counter to zero
+        print type(set_looking_for.item_set)
+        if set_looking_for.item_set.issubset(self.item_set):
+                print 'its a subset!'
 
 ############################################################################
 #                       Parser Function                                    #
@@ -26,29 +29,31 @@ def parser():       #Used to get information and put the data into
 
 #Check for correct amount of parameters
     if len(sys.argv)!=2:
-        print 'Usage: count_digits [FILE]'
+        print 'Usage: apriori.py [FILE]'
         sys.exit(0)
 
     file=open(sys.argv[1],'r')                  # Open the file
 
-    parse_list = []      # create a list that parses the items
+    parse_list = set([])      # create a list that parses the items
     transaction_list = []    # create a list that holds the transactions
 
      # for each line in file:
     for line in file.readlines():
         if line == '\n':
-            if parse_list != []:
+            if parse_list != ([]):
                 # if line is newline and parse_list is empty, make a Transaction
                 # that is passed in the parse_list and put the Transactions in
                 # the transaction_list
                 trans = Transaction(parse_list)
                 transaction_list.append(trans)
-                parse_list = []
+                parse_list = set([])
         else:
             # if line is not newline, change the line into an integer
             # and add it to the parse_list
+
+            print type(parse_list)
             item_read = int(line)
-            parse_list.append(item_read)
+            parse_list.add(item_read)
 
     # add the last Transaction to the transaction list (since for
     # loop exits once it sees an end of file)
@@ -83,6 +88,7 @@ def one_item_sets(T, minsup):
     #for num in item_dict:
     #    print '%i %i' % (num,item_dict[num])
 
+
     return item_dict
 
 
@@ -110,25 +116,33 @@ def frequency_qualifier(item_dict, minsup):
 #                           Generate Function                              #
 ############################################################################
 
-def generate(T,f_item_sets):
+def generate(T,f_item_dict):
 
-    cand_item_set = []      # initialize candidate item set
-    cand_trans_list = []    # initialize the candidate transaction list
+    cand_item_list = set([])      # initialize candidate item set
+    cand_trans_set = set([])    # initialize the candidate transaction list
                             # that holds the candidate item sets
 
-    for itemset in T:
-        for itemset_1 in f_item_sets:
-            cand_item_set = itemset_1
-            for i in range(10):
-                if i not in itemset_1.item_set
-                    cand_item_set.append(i)
-                    trans = Transaction(cand_item_set)
-                    cand_trans_list.append(trans)
-                    print cand_trans_list
-                    cand_item_set = itemset_1
+    for itemset_1 in f_item_dict:
+        cand_item_list = set([])
+        for i in f_item_dict:
+            # for each item or set, pair it with a item that is not the
+            # item or in the set (last part implement later)
+            # put them all in a list called cand_item_set and add the
+            # cand_item_set to the cand_trans_list, which holds all the
+            # cand_item_sets
+            if i != itemset_1:
+                if i > itemset_1:
+                    cand_item_list.add(itemset_1)
+                    cand_item_list.add(i)
+                    trans = Transaction(cand_item_list)
+                    cand_trans_set.add(trans)
+    #                print cand_item_set
+                    cand_item_list = set([])
 
+    #for num in cand_trans_set:
+    #    print num.item_set
 
-
+    return cand_trans_set
 
 ############################################################################
 #                               Main                                       #
@@ -136,6 +150,12 @@ def generate(T,f_item_sets):
 
 transaction_list =  parser()
 #print transaction_list[0].item_set
-item_dict = one_item_sets(transaction_list,2)
+L_kminusone_set = one_item_sets(transaction_list,2)      #
+k = 2
+cand_trans_list = []
 
-generate(transaction_list,item_dict)
+#while L_kminusone_set != {}:
+cand_trans_list = generate(transaction_list,L_kminusone_set)
+for trans in transaction_list:
+    for ctrans in cand_trans_list:
+        trans.subset(ctrans)
