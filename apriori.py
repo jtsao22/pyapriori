9c1c3a8f6493796ddb5b25e83b9063ac1b1308d5
Jason Tsao <jtsao22@furi.(none)>
2009-07-14 16:59:29 -0700

    THE BUG HAS BEEN FIXED. This runs faster than apriori_inefficient. This is the last update before including an extra prune step in generate function.

--- apriori.py
+++ apriori.py
@@ -36,11 +36,6 @@ class Transaction:
 def parser(w_size,file_name,d_wind):   # Used to get information from file
                                         #and put the data into transactions
 
-#Check for correct amount of parameters
-  #  if len(sys.argv)!=2:
-  #      print 'Usage: apriori.py [FILE]'
-  #      sys.exit(0)
-
     file=open(file_name,'r')                  # Open the file
 
     transaction_list = []    # create a list that holds the transactions
@@ -68,21 +63,6 @@ def parser(w_size,file_name,d_wind):   # Used to get information from file
                     iter = 0
             i += 1
     else:
-    #    counter = 0
-    #    parse_list = set([])
-    #    for token in token_list[:-w_size + 1]:
-    #        parse_list.add(token)
-    #        if(counter >= d_wind):
-    #            trans = Transaction(parse_list)
-    #            transaction_list.append(trans)
-    #            parse_list = set([])
-    #            counter = 0
-    #        else:
-    #            counter += 1
-        #token_set = set(token_list)
-        #for token in token_set:
-        #    transaction_list += dyn_search_window(token,token_list)
-
         for index,start_t in enumerate(token_list):
             for ind, token in enumerate(token_list[index:]):
                 if token == start_t and ind != 0:
@@ -103,39 +83,12 @@ def parser(w_size,file_name,d_wind):   # Used to get information from file
         total_window_size += len(trans.item_set)
         if max_window_size < len(trans.item_set):
             max_window_size = len(trans.item_set)
-    print "average window size: ",
+    print "average_window_size: "
     print total_window_size/len(transaction_list)
 
-    print "max window size: ",
-    print max_window_size
+    print "max window size: "
+    print  max_window_size
 
-    #j = 1
-    #for t in transaction_list:
-    #    print j, ": " , t.item_set
-    #    j +=1
-
-    # for each line in file:
-    #for line in file.readlines():
-    #    if line == '\n':
-    #        if parse_list != ([]):
-    #        #if line is newline and parse_list is empty, make a Transaction
-    #        # that is passed in the parse_list and put the Transactions in
-    #        # the transaction_list
-    #            trans = Transaction(parse_list)
-    #            transaction_list.append(trans)
-    #            parse_list = set([])
-    #    else:
-    #        # if line is not newline, change the line into an integer
-    #        # and add it to the parse_list
-    #        item_read = int(line)
-    #        parse_list.add(item_read)
-
-    # add the last Transaction to the transaction list (since for
-    # loop exits once it sees an end of file)
-    #if parse_list != ([]):
-    #    trans = Transaction(parse_list)
-    #    transaction_list.append(trans)
-    print "transaction_list: ", transaction_list
     return transaction_list
 
 #############################################################################
@@ -179,21 +132,6 @@ def one_item_sets(T, minsup): # this function gets L_1 using the
     # initialize item list to empty set
     item_list = []
 
-   # for transaction in T:
-   #     for item in transaction.item_set:
-   #         # Increment the counts of each number 0 to 9 by looking
-   #         # through each item of each transaction in the
-   #         # transaction_list T
-   #         temp = Transaction(set([item]))
-   #         item_is_present = 0
-   #         for iter in item_list:
-   #             if temp.item_set == iter.item_set:
-   #                 iter.count += 1
-   #                 item_is_present = 1
-   #         if item_is_present == 0:
-   #             temp.count = 1
-   #             item_list.append(temp)
-
     all_transactions_list = []
 
     for transaction in T:
@@ -208,20 +146,6 @@ def one_item_sets(T, minsup): # this function gets L_1 using the
         temp.count = all_transactions_list.count(item)
 
         item_list.append(temp)
-
-
-
-#              i in item_list:
-#                    if temp.item_set == i.item_set:
-#                        i.count += 1
-#                        item_is_present = 1
-#                if item_is_present == 0:
-#                    temp.count = 1
-#                    item_list.append(temp)
-#
-#    for i in item_list:
-#        print i.item_set, " has this count: " , i.count
-
 
 
     # call frequency_qualifier to remove items whose counts are < minsup
@@ -271,9 +195,6 @@ def generate(f_item_list,L_1, minsup,ht): # used to find the candidate
             #if not transaction2.item_set.issubset(transaction1.item_set):
             add_numbers_set = transaction2.item_set -\
                     transaction1.item_set
-                #print "trans1.item_set: " , transaction1.item_set
-                #print "trans2.item_set: " , transaction2.item_set
-                #print "add_numbers_list: ", add_numbers_set
             for num in add_numbers_set:
                 cand_item_set = transaction1.item_set.copy()
                 cand_item_set.add(num)
@@ -290,8 +211,6 @@ def generate(f_item_list,L_1, minsup,ht): # used to find the candidate
                     cand_trans_set.add(trans)
                     ht.add_trans(trans)
 
-    #print "ht.prints: ", ht.prints()
-    #print "cand_trans_set: ", cand_trans_set
 
     return cand_trans_set
 
@@ -338,10 +257,6 @@ def apriori(minsup, w_size,file, d_window,node_threshold):
     L_1 = one_item_sets(transaction_list,minsup)
     L_kminusone_set = L_1
 
-    #for i in L_1:
-    #    print i.item_set , " has this count: ", i.count
-
-
     # all_Lk_dict holds all frequent itemsets with item_count mapped to
     # the itemsets
     all_Lk_dict = {}
@@ -360,26 +275,15 @@ def apriori(minsup, w_size,file, d_window,node_threshold):
         # call generate to make the candidate transaction list
 
         ht.reinitialize()
-        #ht = hash_tree.Hash_tree(node_threshold)
         cand_trans_list = generate(L_kminusone_set,L_1,minsup,ht)
-        print "length of cand_trans_list: ", len(cand_trans_list)
-        #print "list: ", cand_trans_list
-        #print "ht.prints(): ", ht.prints()
 
-        print "cand_trans_list: ", cand_trans_list
-        print "ht.prints(): ", ht.prints()
 
         for trans in transaction_list:
             # call Subset to form the final candidate transaction list
             #cand_list_final = Subset(cand_trans_list,trans)
- #           ht.reinitialize()
- #           print "trans: " ,trans
             ht.cand_list_final = []
             ht.subset(trans)
-     #      cand_list_final = ht.cand_list_final
-            print "trans: ", trans
-            print "cand_list_final: " , ht.cand_list_final
-            print "length of cand_list_final: ", len(ht.cand_list_final)
+            debuga(str(len(ht.cand_list_final)))
 
             for candidates in ht.cand_list_final:
                 # increment the count for candidates in the final list
@@ -397,7 +301,8 @@ def apriori(minsup, w_size,file, d_window,node_threshold):
         ht.check_minsup(ht.root,minsup)
         #L_k_set = ht.L_k_set
 
-        print "L_k_set: ", ht.L_k_set
+        debuga("L_k_set: ")
+        debuga(ht.L_k_set)
         if ht.L_k_set != []:
             # add the set to the all_Lk_dict if its not an empty set
             all_Lk_dict[k] = ht.L_k_set
@@ -405,12 +310,13 @@ def apriori(minsup, w_size,file, d_window,node_threshold):
         # increment to the next iteration
 
         L_kminusone_set = ht.L_k_set
-        print "length of L_kminusone_set: ", len(L_kminusone_set)
-        print "L_kminusone_set: " , L_kminusone_set
-        print "length of cand_list_final: ", len(ht.cand_list_final)
-        print "cand_list_Final: ", ht.cand_list_final
+        debuga("length of L_kminusone_set: " +
+                str(len(L_kminusone_set)))
+        debuga("L_kminusone_set: " + str(L_kminusone_set))
+        debuga("cand_list_Final: " + str(ht.cand_list_final))
         k += 1
-        print "___________________new iteration_________________________"
+        debuga("___________________new iteration_____________")
+
 
 
 
@@ -424,6 +330,24 @@ def apriori(minsup, w_size,file, d_window,node_threshold):
             outputfile.write(output_string)
             outputfile.write('\n')
 
+###########################################################################
+#                           Debug Function                                #
+###########################################################################
+
+def debuga(data):
+    if False:
+#        sys.stderr.write(str(data))
+#        sys.stderr.write('\n')
+        print data
+
+def debuga2(data):
+    if True:
+        print data
+
+
+
+
+
 ############################################################################
 #                               Main                                       #
 ############################################################################
@@ -434,7 +358,7 @@ if __name__ == '__main__':
     try:
         opts,args = getopt.getopt(sys.argv[2:], "d")
     except getopt.GetoptError, err:
-        print str(err)
+        debuga(str(err))
         sys.exit(2)
 
     dynamic_window = 0
@@ -445,5 +369,4 @@ if __name__ == '__main__':
             print "Unhandled option"
 
     apriori(3,5,sys.argv[1],dynamic_window,3)
-
 
